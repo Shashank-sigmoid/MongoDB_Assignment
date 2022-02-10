ed3f395c34e3e5dc51ff2d05cf29f3b514cb1514
Shashank-sigmoid <98099928+Shashank-sigmoid@users.noreply.github.com>
2022-02-10 18:55:02 +0530

    Update q4_c_2.py

--- q4_c_2.py
+++ q4_c_2.py
@@ -1,20 +1,25 @@
 from mongodb_assignment.load_theaters_data import theaters_collection
 
+lat = float(input("Enter the latitude: "))
+lng = float(input("Enter the longitude: "))
+
+theaters_collection.create_index({"location": "2dsphere"})
+theaters_collection.find().sort("_id", -1)
+
 # Top 10 closest theaters to a given coordinates
-var = theaters_collection.aggregate([
+pipeline = [
     {
-        "location": {
-            "$near": {
-                "$geometry": {
-                    "type": "Point",
-                    "coordinates": [25.087626, 55.151134]
-                },
-            }
-        }
+        "$geoNear": {"near": {"type": "Point", "coordinates": [lat, lng]},
+                     "maxDistance": 1000000, "distanceField": "distance"}
+    },
+    {
+        "$project": {"location.address.city": 1, "_id": 0, "location.geo.coordinates": 1}
     },
     {
         "$limit": 10
-    }]
-)
-for result in var:
+    }
+]
+
+results = theaters_collection.aggregate(pipeline)
+for result in results:
     print(result)
